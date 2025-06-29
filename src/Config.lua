a127e539389a618ec8e98cc6a8d42c518e2e407d
xl-spooky <romansebi64@gmail.com>
2025-06-30 02:03:43 +0300

    fix: low lever executor, need to make all in one file

--- src/Config.lua
+++
@@ -1,7 +0,0 @@
-local Config = {
-    AutoHarvest     = false,    -- starts disabled
-    HarvestInterval = 0.1,      -- seconds between harvest cycles
-    Running         = true,     -- set false to exit loop
-}
-
-return Config

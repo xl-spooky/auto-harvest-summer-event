afb88d19c3cace8a70cffa5f6f5e93fd6f2dee6d
xl-spooky <romansebi64@gmail.com>
2025-06-30 01:41:49 +0300

    fix: startup

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
 local baseUrl = "https://raw.githubusercontent.com/xl-spooky/auto-harvest-summer-event/main/src/"
 
-local mainCode = game:HttpGet(baseUrl .. "Main.lua")
+local mainCode = game:HttpGet(baseUrl .. "Main.lua", true)
 loadstring(mainCode)()

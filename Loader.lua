d4cc83e3478e40c96b19e429f992be5e7128ae56
xl-spooky <romansebi64@gmail.com>
2025-06-30 01:47:50 +0300

    debug: GUI on loader

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,12 @@
 local baseUrl = "https://raw.githubusercontent.com/xl-spooky/auto-harvest-summer-event/main/src/"
 
+local Players = game:GetService("Players")
+local gui = Instance.new("ScreenGui", Players.LocalPlayer.PlayerGui)
+local lbl = Instance.new("TextLabel", gui)
+lbl.Size = UDim2.new(0,200,0,50)
+lbl.Position = UDim2.new(0,10,0,10)
+lbl.Text = "Loader OK"
+lbl.BackgroundTransparency = 1
+
 local mainCode = game:HttpGet(baseUrl .. "Main.lua", true)
 loadstring(mainCode)()

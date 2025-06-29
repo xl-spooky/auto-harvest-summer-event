53006ca0bc1879ed47c04f8499ab5a7e80126e08
xl-spooky <romansebi64@gmail.com>
2025-06-29 23:12:44 +0300

    fix: pointing Loader.lua to our source main entry point

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
--- Paste this into your executor to kick everything off:
-loadstring(
-    game:HttpGet("https://raw.githubusercontent.com/xl-spooky/auto-harvest-summer-event/main/Loader.lua")
-)()
+local baseUrl = "https://raw.githubusercontent.com/xl-spooky/auto-harvest-summer-event/main/src/"
+
+local mainCode = game:HttpGet(baseUrl .. "Main.lua")
+loadstring(mainCode)()
